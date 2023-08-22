59d6e41f68728bf020166570b4d8bf348f367a5b
Hasnain <73098513+Shaikhasnain@users.noreply.github.com>
2023-08-22 14:17:55 +0000

    semicolon added to the js statement in let_const_var file

--- 01_basic/01_let_const_var.js
+++ 01_basic/01_let_const_var.js
@@ -1,11 +1,11 @@
-const userId = 32351
-let emailAddress = "demo@gmail.com"
-var userName = "Demo User"
+const userId = 32351;
+let emailAddress = "demo@gmail.com";
+var userName = "Demo User";
 cityName = "Surat";
 
-emailAddress = "demo.live@gmail.com"
+emailAddress = "demo.live@gmail.com";
 
 /* don't use var keyword because of 
 this is not a compitable for block scope and function scope */
 
-console.table([userId, emailAddress, userName, cityName])
+console.table([userId, emailAddress, userName, cityName]);
