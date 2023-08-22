3725ac9f7b1b488569cb709c827a8038854b3031
Hasnain <73098513+Shaikhasnain@users.noreply.github.com>
2023-08-22 14:13:07 +0000

    let_const_var file added

---
+++ 01_basic/01_let_const_var.js
@@ -0,0 +1,11 @@
+const userId = 32351
+let emailAddress = "demo@gmail.com"
+var userName = "Demo User"
+cityName = "Surat";
+
+emailAddress = "demo.live@gmail.com"
+
+/* don't use var keyword because of 
+this is not a compitable for block scope and function scope */
+
+console.table([userId, emailAddress, userName, cityName])
