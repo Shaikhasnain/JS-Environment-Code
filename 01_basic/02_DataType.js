7509d3acf6859a374709821a160605da1fabddd0
Hasnain <73098513+Shaikhasnain@users.noreply.github.com>
2023-08-23 10:07:06 +0000

    files names are updated

---
+++ 01_basic/02_DataType.js
@@ -0,0 +1,22 @@
+// number
+// string
+// undefined
+// null
+// bigInt
+// symbol
+// boolean
+
+// Object
+
+let username = "John Lee"
+let age = 25
+let isLoggedIn = true;
+let address;
+let mobileNo = null
+
+console.log(typeof username) // Type is string
+console.log(typeof age) // Type is number
+console.log(typeof isLoggedIn) // Type is boolean
+console.log(typeof address) // Type is undefined
+console.log(typeof mobileNo) // Type is object
+console.log(null == 0);
---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,37 @@
+let score = undefined
+let scoreInNum = Number(score)
+// console.log(typeof score);
+// console.log(scoreInNum);
+
+
+// null => 0
+// true => 1
+// false => 0
+// undefined => NaN
+// "125" => 125
+// "125mdd" => NaN
+
+let checkedBoolean = 1
+let isBooleanValue = Boolean(checkedBoolean)
+// console.log(isBooleanValue)
+
+// "" => False
+// "Hello World" => True
+// 0 => False
+// 1 => True
+
+// let x = 3;
+// const y = x++;
+
+// console.log(`x:${x}, y:${y}`);
+// // Expected output: "x:4, y:3"
+
+// let a = 3;
+// const b = ++a;
+
+// console.log(`a:${a}, b:${b}`);
+// // Expected output: "a:4, b:4"
+
+for (let x = 0; x<5; x++) {
+    console.log(x);
+}
