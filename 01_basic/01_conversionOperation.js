6b39b503bcd2f04fe31573a85931a96be2ab5f8e
Hasnain <73098513+Shaikhasnain@users.noreply.github.com>
2023-08-23 07:25:44 +0000

    Conversion Operation

---
+++ 01_basic/01_conversionOperation.js
@@ -0,0 +1,21 @@
+let score = undefined
+let scoreInNum = Number(score)
+// console.log(typeof score);
+// console.log(scoreInNum);
+
+
+// null => 0
+// true => 1
+// false => 0
+// undefined => NaN
+// "125" => 125
+// "125mdd" => NaN
+
+let checkedBoolean = 1
+let isBooleanValue = Boolean(checkedBoolean)
+console.log(isBooleanValue)
+
+// "" => False
+// "Hello World" => True
+// 0 => False
+// 1 => True
